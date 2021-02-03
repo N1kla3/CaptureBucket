d034e1f4d324b762b7216e64046546ea26064704
Nicola <kolya.vladimirsky@gmail.com>
2021-02-03 10:38:13 +0300

    health and magic

--- Source/CaptureBucket/CaptureBucketCharacter.cpp
+++ Source/CaptureBucket/CaptureBucketCharacter.cpp
@@ -10,6 +10,10 @@
 #include "GameFramework/SpringArmComponent.h"
 #include "HeadMountedDisplayFunctionLibrary.h"
 #include "Materials/Material.h"
+#include "Components/BoxComponent.h"
+#include "Components/TimelineComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "TimerManager.h"
 #include "Engine/World.h"
 
 ACaptureBucketCharacter::ACaptureBucketCharacter()
@@ -29,39 +33,67 @@ ACaptureBucketCharacter::ACaptureBucketCharacter()
 	GetCharacterMovement()->bSnapToPlaneAtStart = true;
 
 	// Create a camera boom...
-	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->SetUsingAbsoluteRotation(true); // Don't want arm to rotate when character does
-	CameraBoom->TargetArmLength = 800.f;
-	CameraBoom->SetRelativeRotation(FRotator(-60.f, 0.f, 0.f));
-	CameraBoom->bDoCollisionTest = false; // Don't want to pull camera in when it collides with level
+	m_CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	m_CameraBoom->SetupAttachment(RootComponent);
+	m_CameraBoom->SetUsingAbsoluteRotation(true); // Don't want arm to rotate when character does
+	m_CameraBoom->TargetArmLength = 800.f;
+	m_CameraBoom->SetRelativeRotation(FRotator(-60.f, 0.f, 0.f));
+	m_CameraBoom->bDoCollisionTest = false; // Don't want to pull camera in when it collides with level
 
 	// Create a camera...
-	TopDownCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("TopDownCamera"));
-	TopDownCameraComponent->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
-	TopDownCameraComponent->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+	m_TopDownCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("TopDownCamera"));
+	m_TopDownCameraComponent->SetupAttachment(m_CameraBoom, USpringArmComponent::SocketName);
+	m_TopDownCameraComponent->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
 
 	// Create a decal in the world to show the cursor's location
-	CursorToWorld = CreateDefaultSubobject<UDecalComponent>("CursorToWorld");
-	CursorToWorld->SetupAttachment(RootComponent);
+	m_CursorToWorld = CreateDefaultSubobject<UDecalComponent>("CursorToWorld");
+	m_CursorToWorld->SetupAttachment(RootComponent);
 	static ConstructorHelpers::FObjectFinder<UMaterial> DecalMaterialAsset(TEXT("Material'/Game/TopDownCPP/Blueprints/M_Cursor_Decal.M_Cursor_Decal'"));
 	if (DecalMaterialAsset.Succeeded())
 	{
-		CursorToWorld->SetDecalMaterial(DecalMaterialAsset.Object);
+		m_CursorToWorld->SetDecalMaterial(DecalMaterialAsset.Object);
 	}
-	CursorToWorld->DecalSize = FVector(16.0f, 32.0f, 32.0f);
-	CursorToWorld->SetRelativeRotation(FRotator(90.0f, 0.0f, 0.0f).Quaternion());
+	m_CursorToWorld->DecalSize = FVector(16.0f, 32.0f, 32.0f);
+	m_CursorToWorld->SetRelativeRotation(FRotator(90.0f, 0.0f, 0.0f).Quaternion());
 
 	// Activate ticking in order to update the cursor every frame.
 	PrimaryActorTick.bCanEverTick = true;
 	PrimaryActorTick.bStartWithTickEnabled = true;
 }
 
+void ACaptureBucketCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	m_FullHealth = 1000.f;
+	m_Health = m_FullHealth;
+	m_HealthPercentage = 1.f;
+	m_PreviousHealth = m_HealthPercentage;
+	SetCanBeDamaged(true);
+
+	m_FullMagic = 100.f;
+	m_Magic = m_FullMagic;
+	m_MagicPercentage = 1.f;
+	m_PreviousMagic = m_MagicPercentage;
+	bCanUseMagic = true;
+
+	if (m_MagicCurve)
+	{
+		FOnTimelineFloat timeline_callback;
+		FOnTimelineEventStatic timeline_finished_callback;
+		timeline_callback.BindUFunction(this, FName("SetMagicValue"));
+		timeline_finished_callback.BindUFunction(this, FName("SetMagicState"));
+		m_MyTimeline.AddInterpFloat(m_MagicCurve, timeline_callback);
+		m_MyTimeline.SetTimelineFinishedFunc(timeline_finished_callback);
+	}
+}
+
+
 void ACaptureBucketCharacter::Tick(float DeltaSeconds)
 {
     Super::Tick(DeltaSeconds);
 
-	if (CursorToWorld != nullptr)
+	if (m_CursorToWorld != nullptr)
 	{
 		if (UHeadMountedDisplayFunctionLibrary::IsHeadMountedDisplayEnabled())
 		{
@@ -69,12 +101,12 @@ void ACaptureBucketCharacter::Tick(float DeltaSeconds)
 			{
 				FHitResult HitResult;
 				FCollisionQueryParams Params(NAME_None, FCollisionQueryParams::GetUnknownStatId());
-				FVector StartLocation = TopDownCameraComponent->GetComponentLocation();
-				FVector EndLocation = TopDownCameraComponent->GetComponentRotation().Vector() * 2000.0f;
+				FVector StartLocation = m_TopDownCameraComponent->GetComponentLocation();
+				FVector EndLocation = m_TopDownCameraComponent->GetComponentRotation().Vector() * 2000.0f;
 				Params.AddIgnoredActor(this);
 				World->LineTraceSingleByChannel(HitResult, StartLocation, EndLocation, ECC_Visibility, Params);
 				FQuat SurfaceRotation = HitResult.ImpactNormal.ToOrientationRotator().Quaternion();
-				CursorToWorld->SetWorldLocationAndRotation(HitResult.Location, SurfaceRotation);
+				m_CursorToWorld->SetWorldLocationAndRotation(HitResult.Location, SurfaceRotation);
 			}
 		}
 		else if (APlayerController* PC = Cast<APlayerController>(GetController()))
@@ -83,8 +115,71 @@ void ACaptureBucketCharacter::Tick(float DeltaSeconds)
 			PC->GetHitResultUnderCursor(ECC_Visibility, true, TraceHitResult);
 			FVector CursorFV = TraceHitResult.ImpactNormal;
 			FRotator CursorR = CursorFV.Rotation();
-			CursorToWorld->SetWorldLocation(TraceHitResult.Location);
-			CursorToWorld->SetWorldRotation(CursorR);
+			m_CursorToWorld->SetWorldLocation(TraceHitResult.Location);
+			m_CursorToWorld->SetWorldRotation(CursorR);
 		}
 	}
+}
+
+float ACaptureBucketCharacter::GetHealth() const
+{
+	return m_HealthPercentage;
+}
+
+float ACaptureBucketCharacter::GetMagic() const
+{
+	return m_MagicPercentage;
+}
+
+void ACaptureBucketCharacter::UpdateHealth(float HealthChange)
+{
+	
+}
+
+void ACaptureBucketCharacter::UpdateMagic(float MagicChange)
+{
+	
+}
+
+FText ACaptureBucketCharacter::GetHealthIntText() const
+{
+	int hp = FMath::RoundHalfFromZero(m_HealthPercentage * 100);
+	FString hp_string = FString::FromInt(hp);
+	FString health_hud = hp_string + FString(TEXT("%"));
+	FText result = FText::FromString(health_hud);
+	return result;
+}
+
+FText ACaptureBucketCharacter::GetMagicIntText() const
+{
+	int magic = FMath::RoundHalfFromZero(m_MagicPercentage * 100);
+	FString magic_string = FString::FromInt(magic);
+	FString magic_hud = magic_string + FString(TEXT("%"));
+	FText result = FText::FromString(magic_hud);
+	return result;
+}
+
+void ACaptureBucketCharacter::DamageTimer()
+{
+}
+
+void ACaptureBucketCharacter::SetDamageState()
+{
+}
+
+void ACaptureBucketCharacter::SetMagicValue()
+{
+}
+
+void ACaptureBucketCharacter::SetMagicState()
+{
+}
+
+void ACaptureBucketCharacter::SetMagicChange(float MagicChange)
+{
+}
+
+bool ACaptureBucketCharacter::PlayFlash()
+{
+	return true;
 }
--- Source/CaptureBucket/CaptureBucketCharacter.h
+++ Source/CaptureBucket/CaptureBucketCharacter.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Components/TimelineComponent.h"
 #include "CaptureBucketCharacter.generated.h"
 
 UCLASS(Blueprintable)
@@ -13,28 +14,109 @@ class ACaptureBucketCharacter : public ACharacter
 
 public:
 	ACaptureBucketCharacter();
+	
+	UFUNCTION(BlueprintPure, Category = "Health")
+        float GetHealth() const;
 
-	// Called every frame.
+	UFUNCTION(BlueprintPure, Category = "Magic")
+    float GetMagic() const;
+
+	UFUNCTION(BlueprintCallable, Category = "Health")
+    void UpdateHealth(float HealthChange);
+
+	UFUNCTION(BlueprintCallable, Category = "Magic")
+    void UpdateMagic(float MagicChange);
+
+	UFUNCTION(BlueprintPure, Category = "Health")
+    FText GetHealthIntText() const;
+
+	UFUNCTION(BlueprintPure, Category = "Magic")
+    FText GetMagicIntText() const;
+
+	UFUNCTION()
+    void DamageTimer();
+
+	UFUNCTION()
+    void SetDamageState();
+
+	UFUNCTION()
+    void SetMagicValue();
+
+	UFUNCTION()
+    void SetMagicState();
+
+	UFUNCTION()
+    void SetMagicChange(float MagicChange);
+
+	UFUNCTION(BlueprintPure, Category = "Health")
+    bool PlayFlash();
+
 	virtual void Tick(float DeltaSeconds) override;
+	
+	FORCEINLINE class UCameraComponent* GetTopDownCameraComponent() const { return m_TopDownCameraComponent; }
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return m_CameraBoom; }
+	FORCEINLINE class UDecalComponent* GetCursorToWorld() { return m_CursorToWorld; }
+	
+	UPROPERTY(EditAnywhere, Category = "Magic")
+	class UMaterialInterface* GunDefaultMaterial;
 
-	/** Returns TopDownCameraComponent subobject **/
-	FORCEINLINE class UCameraComponent* GetTopDownCameraComponent() const { return TopDownCameraComponent; }
-	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-	/** Returns CursorToWorld subobject **/
-	FORCEINLINE class UDecalComponent* GetCursorToWorld() { return CursorToWorld; }
+	UPROPERTY(EditAnywhere, Category = "Magic")
+	class UMaterialInterface* GunOverheatMaterial;
 
-private:
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health")
+	float m_FullHealth;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health")
+	float m_Health;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health")
+	float m_HealthPercentage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health")
+	float m_PreviousHealth;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health")
+	float m_FullMagic;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Magic")
+	float m_Magic;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Magic")
+	float m_MagicPercentage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Magic")
+	float m_PreviousMagic;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Magic")
+	float m_MagicValue;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health")
+	float m_RedFlash;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Magic")
+	UCurveFloat *m_MagicCurve;
+	
 	/** Top down camera */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* TopDownCameraComponent;
+	class UCameraComponent* m_TopDownCameraComponent;
 
 	/** Camera boom positioning the camera above the character */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* CameraBoom;
+	class USpringArmComponent* m_CameraBoom;
 
 	/** A decal that projects to the cursor location. */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class UDecalComponent* CursorToWorld;
+	class UDecalComponent* m_CursorToWorld;
+	
+	FTimeline m_MyTimeline;
+	float m_CurveFloatValue;
+	float m_TimelineValue;
+	FTimerHandle m_MemberTimerHandle;
+
+	bool bCanUseMagic;
 };
+
 
