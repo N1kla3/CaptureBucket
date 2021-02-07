b90f560cc559bdb7a7cd7588925430c5e9b57f67
Nicola <kolya.vladimirsky@gmail.com>
2021-02-07 12:52:53 +0300

    mana bar

---
+++ Source/CaptureBucket/UI/ManaBarWidget.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ManaBarWidget.h"
+
+
+UManaBarWidget::UManaBarWidget(const FObjectInitializer& ObjectInitializer) :
+	Super(ObjectInitializer)
+{
+	
+}
+
+void UManaBarWidget::UpdateManaBar(float Percentage)
+{
+}
+
+void UManaBarWidget::SetText(float Percentage)
+{
+	//TODO logic
+}
